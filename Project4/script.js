b301cbe51a8cd0cfe45a4813cbfc7498018b5f59
Chris Castellano <c.castellano18@gmail.com>
2022-05-02 00:19:01 -0500

    exchanger

---
+++ Project4/script.js
@@ -0,0 +1,38 @@
+const currencyElement_one = document.getElementById('currency-one');
+const currencyElement_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//fetch exchange rates
+function calculate() {
+  const currency_one = currencyElement_one.value;
+  const currency_two = currencyElement_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[currency_two];
+
+      rateEl.innerHTML = `1${currency_one} = ${rate}${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+//event listeners
+currencyElement_one.addEventListener('change', calculate);
+currencyElement_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyElement_one.value;
+
+  currencyElement_one.value = currencyElement_two.value;
+  currencyElement_two.value = temp;
+  calculate();
+});
+
+calculate();
